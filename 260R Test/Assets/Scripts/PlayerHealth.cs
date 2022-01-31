0c71c57267702b3b0e4fa16626ef6984726477ad
Miriam Jardine <miajardine1@gmail.com>
2022-01-31 06:46:43 -0700

    Health Interface

---
+++ 260R Test/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IHealth
+{
+    public float Health { get; set;}
+    
+    public void Start()
+    {
+    throw new System.NotImplementedException();
+    }
+    
+    public void Run()
+    {
+    throw new System.NotImplementedException();
+    }
+    
+}
