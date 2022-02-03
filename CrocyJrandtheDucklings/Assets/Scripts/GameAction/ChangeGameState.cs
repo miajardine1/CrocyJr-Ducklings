fec185e8c248ef3f96f09bf7944883524af93bb3
Miriam Jardine <miajardine1@gmail.com>
2022-02-03 11:32:04 -0700

    CrocyJr GameActions

---
+++ CrocyJrandtheDucklings/Assets/Scripts/GameAction/ChangeGameState.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGameState : MonoBehaviour
+{
+   public void EndGame()
+    {
+        print(message:"GameOver");
+    }
+}
---
+++ CrocyJrandtheDucklings/Assets/Scripts/GameAction/ChangeScore.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScore : MonoBehaviour
+{
+    public void UpdateScore()
+    {
+        print(message:"ScoreUpdated");
+    }
+}
