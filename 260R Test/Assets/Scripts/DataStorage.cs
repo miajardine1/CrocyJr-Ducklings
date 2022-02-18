20f1b39f6b7fcc7ee7830f0935f43b1a19d6b65f
Miriam Jardine <miajardine1@gmail.com>
2022-02-18 07:15:39 -0700

    Data Storage

---
+++ 260R Test/Assets/Scripts/DataStorage.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataStorage", menuName = "Utilites/Data Storage Object")]
+public class DataStorage : ScriptableObject
+{
+    public ScriptableObject data;
+    public List<ScriptableObject> listData;
+
+    public void SetListData()
+    {
+        foreach (var obj in listData)
+        {
+            SetData(obj);
+        }
+    }
+
+    public void GetListData()
+    {
+        foreach (var obj in listData)
+        {
+            GetData(obj);
+        }
+    }
+    public void SetData(ScriptableObject obj)
+    {
+        if (obj == null) return;
+        PlayerPrefs.SetString(data.name, JsonUtility.ToJson(data));
+    }
+
+    public void GetData(ScriptableObject obj)
+    {
+        if (obj == null) return;
+        if (!string.IsNullOrEmpty((PlayerPrefs.GetString(obj.name))))
+            JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(obj.name), obj);
+    }
+
+    public void GetData()
+    {
+        if (data == null) return;
+        if(!string.IsNullOrEmpty(PlayerPrefs.GetString(data.name)))
+            JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(data.name), data);
+    }
+}
