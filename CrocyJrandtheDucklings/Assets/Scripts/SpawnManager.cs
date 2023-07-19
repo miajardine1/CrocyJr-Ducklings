bbcca9d44218817b3a6fcabec13d6d0f923acfbc
Miriam <miajardine1@gmail.com>
2023-07-18 21:04:33 -0600

    Final adjustments

--- CrocyJrandtheDucklings/Assets/Scripts/SpawnManager.cs
+++ CrocyJrandtheDucklings/Assets/Scripts/SpawnManager.cs
@@ -20,7 +20,7 @@ public class SpawnManager : MonoBehaviour
   public void SpawnDuck()
     {
         Vector3 spawnPoint = new Vector3(Random.Range(-2f,2f),7.5f,-1);
-        spawnTime = Mathf.MoveTowards(spawnTime, 125f, .3f);
+        spawnTime = Mathf.MoveTowards(spawnTime, 125f, .5f);
         Instantiate(duck, spawnPoint, Quaternion.identity);
     }
 
